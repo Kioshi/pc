ec0c9e66d67944656d3c37a7716f4161e8ced8aa
Kioshi <Kioshi@a.b>
2016-12-13 00:34:53 +0100

    basdadfhasthfseheargk mdaskjdf dhm ldhnmmerlh,!!!

---
+++ fssim/zip/array.h
@@ -0,0 +1,65 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+/**
+* @file    array.h
+* @author  Stepan Martinek <smartine@students.zcu.cz>
+*
+* @brief Contains Array struct and functions that manipulate with it
+*/
+
+typedef struct array
+{
+    int size;
+    char** string;
+} Array;
+
+
+/**
+* @fn Array* createArray();
+*
+* @brief Allocate and clean Array struct
+*
+* @return Array struct
+*/
+Array* createArray();
+
+/**
+* @fn Array* toArray(char* string);
+*
+* @brief Parse string to array of substrings by delimiter /
+*
+* @param string Field of chars to parse
+* @return Array struct
+*/
+Array* toArray(char* string);
+
+/**
+* @fn void push_back(Array* words, char* s);
+*
+* @brief Append string to end of Array
+*
+* @param words Array struct containing array of strings.
+* @param s Field of characters.
+*/
+void push_back(Array* words, char* s);
+
+/**
+* @fn void pop_front(Array* words);
+*
+* @brief Remove first item from Array
+*
+* @param words Array struct containing array of strings.
+*/
+void pop_front(Array* words);
+
+/**
+* @fn void deleteWords(Array* words);
+*
+* @brief Free every resource of Array
+*
+* @param words Array struct containing array of strings.
+*/
+void deleteWords(Array* words);
+
+#endif /* ARRAY_H */
---
+++ fssim/zip/util.h
@@ -0,0 +1,61 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+
+/**
+* @file    util.h
+* @author  Stepan Martinek <smartine@students.zcu.cz>
+*
+* @brief Contains utility enum, macros and calloc, malloc wrappers
+*/
+
+/* Disable Visual studio warning propagating their functions */
+#pragma warning(disable: 4996) /* _CRT_SECURE_NO_WARNINGS */
+
+/* Define for easier transfer from c++ */
+#define nullptr 0
+
+/* Typedef for easier transfer from c++ */
+typedef enum
+{
+    false = 0,
+    true
+} bool;
+
+/**
+* @fn void* _calloc(size_t count, size_t size);
+*
+* @brief Wrapper for calloc that will terminate program with proper error code if no memory is available
+*
+* @param count Count of sized blocks
+* @param size Size of allocated block
+* @return Pointer to newly allocated memory
+*/
+void* _calloc(size_t count, size_t size);
+
+/**
+* @fn void* _malloc(size_t size);
+*
+* @brief Wrapper for malloc that will terminate program with proper error code if no memory is available
+*
+* @param size Size of newly allocated memory
+* @return Pointer to newly allocated memory
+*/
+void* _malloc(size_t size);
+
+/* Return codes, for fatal errors */
+enum returnCodes
+{
+    OK = 0,
+    MALLOC_FAIL,
+    WRONG_ARGUMENTS,
+    NO_FILESYSTEM,
+    NO_COMMANDS
+};
+
+#endif /* UTIL_H */
